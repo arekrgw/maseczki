0f542de101129a34c4fcaccf552fbe4d11c28abe
Bartosz <228736@edu.p.lodz.pl>
2021-11-02 15:30:57 +0100

    b
    "working" mouth detection

--- include/MouthDetection.h
+++ include/MouthDetection.h
@@ -1,5 +1,22 @@
+#pragma once
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
 class MouthDetection
 {
+
+private:
+	CascadeClassifier mouthCascade;
+	void loadMouthCascade();
+	void findAllMouth(Mat& image, std::vector<Rect>& mouths);
+	int findIndexOfBestMouth(Mat& image, std::vector<Rect>& mouths);
+
 public:
-  cv::Mat detect(cv::Mat face);
-}
+	MouthDetection();
+	int detect(Mat& image, Rect& coords);
+};
---
+++ src/MouthDetection.cpp
@@ -0,0 +1,63 @@
+#include "MouthDetection.h"
+#include "Resources.h"
+#include <iostream>
+#include <cmath>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/opencv.hpp>
+
+MouthDetection::MouthDetection() {
+	loadMouthCascade();
+}
+
+int MouthDetection::detect(Mat& image, Rect& coords)
+{
+	if (image.empty())
+	{
+		throw std::invalid_argument("Image not provided");
+	}
+
+	std::vector<Rect> mouths;
+
+	findAllMouth(image, mouths);
+
+	if (!mouths.size())
+		return 1;
+	int indexOfBestMouth = findIndexOfBestMouth(image, mouths);
+
+	coords = mouths[indexOfBestMouth];
+
+	return 0;
+}
+
+void MouthDetection::findAllMouth(Mat& image, std::vector<Rect>& mouths)
+{
+	mouthCascade.detectMultiScale(image, mouths);
+}
+
+int MouthDetection::findIndexOfBestMouth(Mat& image, std::vector<Rect>& mouths)
+{
+	int imgCenterY = image.cols * 0.5;
+	float minDistance = 999999999;
+	int bestMouth = -1;
+	for (int i = 0; i < mouths.size(); ++i)
+	{
+		int centerY = mouths[i].x + mouths[i].width * 0.5;
+		int centerX = mouths[i].y + mouths[i].height * 0.5;
+
+		float distance = sqrt(pow(imgCenterY - centerY, 2));
+
+		if (distance < minDistance)
+		{
+			minDistance = distance;
+			bestMouth = i;
+		}
+	}
+
+
+	return bestMouth;
+}
+
+void MouthDetection::loadMouthCascade()
+{
+	mouthCascade.load(MOUTH_CASCADE);
+}
