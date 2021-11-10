c0388a807be2ef92da81871237799fec785f3cd3
Arkadiusz Pawlak <pk-23@sr.dmcs.pl>
2021-11-10 14:51:52 +0100

    formatting, small fixes

--- include/MaskColor.h
+++ include/MaskColor.h
@@ -2,17 +2,17 @@
 #include "Resources.h"
 #include <iostream>
 #include <cmath>
-#include <opencv2/core/base.hpp>
-#include <opencv2/core/mat.hpp>
+#include <opencv2/opencv.hpp>
+
 using namespace cv;
 
-class MaskColor {
-	private:
-		Mat upperHalf(Mat& image);
-		Mat lowerHalf(Mat& image);
-		Scalar count(Mat& image);
+class MaskColor
+{
+private:
+	Mat upperHalfFn(Mat &image);
+	Mat lowerHalfFn(Mat &image);
+	Scalar count(Mat &image);
 
-	public:
-		int detect(Mat& image);
-
+public:
+	int detect(Mat &image);
 };
--- src/MaskColor.cpp
+++ src/MaskColor.cpp
@@ -2,46 +2,55 @@
 #include "Resources.h"
 #include <iostream>
 #include <cmath>
-#include <opencv2/core/base.hpp>
-#include <opencv2/core/mat.hpp>
 #include "Painter.h"
+#include <opencv2/opencv.hpp>
 
 #include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 
-int MaskColor::detect(Mat& image) {
+int MaskColor::detect(Mat &image)
+{
 	if (image.empty())
 	{
 		throw std::invalid_argument("Image not provided");
 	}
-cv:Mat imgToFindFace2;
-	cv::resize(image, imgToFindFace2, cv::Size(), 0.2, 0.2);
-	Mat upperHalf = MaskColor::upperHalf(imgToFindFace2);
-	Mat lowerHalf = MaskColor::lowerHalf(imgToFindFace2);
 
-	Scalar countUpper = MaskColor::count(upperHalf);
-	Scalar countLower = MaskColor::count(lowerHalf);
-	int color=0;
-	for (int i = 0; i < 3; i++) {
-		if (countUpper[i] - countLower[i] < 0) color += countLower[i] - countUpper[i];
-		else color +=countUpper[i] - countLower[i];
+	Mat imgToFindFace2;
+	resize(image, imgToFindFace2, Size(), 0.2, 0.2);
+	Mat upperHalf = upperHalfFn(imgToFindFace2);
+	Mat lowerHalf = lowerHalfFn(imgToFindFace2);
+
+	Scalar countUpper = count(upperHalf);
+	Scalar countLower = count(lowerHalf);
+	int color = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		if (countUpper[i] - countLower[i] < 0)
+			color += countLower[i] - countUpper[i];
+		else
+			color += countUpper[i] - countLower[i];
 	}
 	color /= 3;
-	if (color >= 30) return 0;
+	if (color >= 30)
+		return 0;
 	return 1;
 }
 
-
-Mat MaskColor::upperHalf(Mat& image) {
+Mat MaskColor::upperHalfFn(Mat &image)
+{
 	Mat croppedImage = image(Rect(0, 0, image.cols, image.rows / 2));
 	return croppedImage;
 }
-Mat MaskColor::lowerHalf(Mat& image) {
+
+Mat MaskColor::lowerHalfFn(Mat &image)
+{
 	Mat croppedImage = image(Rect(0, image.rows / 2, image.cols, image.rows / 2));
 	return croppedImage;
 }
-Scalar MaskColor::count(Mat& image) {
+
+Scalar MaskColor::count(Mat &image)
+{
 	Scalar tab;
 	tab = mean(image);
 	return tab;
